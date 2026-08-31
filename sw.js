50232499671f64638c0e7131579da2023c984bf6
zAmo7 <104038459+zAmo7@users.noreply.github.com>
2026-08-31 12:35:53 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE="ghost-hunter-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
